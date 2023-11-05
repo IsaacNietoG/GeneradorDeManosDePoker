4e3449961a964e5d2fbd4b02349cae139842e75d
IsaacNietoG <90740831+IsaacNietoG@users.noreply.github.com>
2023-11-05 13:20:51 -0600

    refactor: Agrega los placeholders y la estructura para los checks

--- GeneradorDeManos.java
+++ GeneradorDeManos.java
@@ -18,6 +18,78 @@
    - Crear el metodo checkPareja
  */
 public class GeneradorDeManos {
-    
-    
+    public static void main(String[] args){
+        System.out.println("Bienvenido al generador de manos de Poker.");
+        System.out.println("Se generará una mano de cartas de Poker aleatorizada y se te dirá si recibiste alguna mano buena.");
+
+        Mano manoGenerada = new Mano();
+        int manoEncontrada=0;
+        switch(9){
+        case 9:
+            manoEncontrada = checkEscaleraReal(manoGenerada);
+            if(manoEncontrada !=0) break;
+        case 8:
+            manoEncontrada = checkEscaleraColor(manoGenerada);
+            if(manoEncontrada !=0) break;
+        case 7:
+            manoEncontrada = checkPoker(manoGenerada);
+            if (manoEncontrada != 0) break;
+        case 6:
+            manoEncontrada = checkFull(manoGenerada);
+            if (manoEncontrada != 0) break;
+        case 5:
+            manoEncontrada = checkColor(manoGenerada);
+            if (manoEncontrada != 0) break;
+        case 4:
+            manoEncontrada = checkEscalera(manoGenerada);
+            if (manoEncontrada != 0) break;
+        case 3:
+            manoEncontrada = checkTrio(manoGenerada);
+            if (manoEncontrada != 0) break;
+        case 2:
+            manoEncontrada = checkDoblePareja(manoGenerada);
+            if (manoEncontrada != 0) break;
+        case 1:
+            manoEncontrada = checkPareja(manoGenerada);
+            if (manoEncontrada != 0) break;
+        case 0:
+            manoEncontrada = 0;
+        }
+    }
+
+    public static int checkEscaleraReal(Mano mano){
+        
+    }
+
+    public static int checkEscaleraColor(Mano mano){
+        
+    }
+
+    public static int checkPoker(Mano mano) {
+
+    }
+
+    public static int checkFull(Mano mano) {
+
+    }
+
+    public static int checkColor(Mano mano) {
+
+    }
+
+    public static int checkEscalera(Mano mano) {
+
+    }
+
+    public static int checkTrio(Mano mano) {
+
+    }
+
+    public static int checkDoblePareja(Mano mano) {
+
+    }
+
+    public static int checkPareja(Mano mano) {
+
+    }
 }
